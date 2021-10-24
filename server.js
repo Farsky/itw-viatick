5755ecfd61216f9dedfe274e1e6381e559012c7a
NGUYEN Vien Thien <vnguyen2@mantu.com>
2021-10-25 00:17:24 +0700

    Allow static file serving

--- server.js
+++ server.js
@@ -17,16 +17,25 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const db = require("./app/models");
-db.sequelize.sync({ force: true }).then(() => {
-    console.log("Drop and re-sync db.");
-});
+// express on its own has no notion
+// of a "file". The express.static()
+// middleware checks for a file matching
+// the `req.path` within the directory
+// that you pass it. In this case "GET /js/app.js"
+// will look for "./assets/js/app.js".
+app.use(express.static(path.join(__dirname, 'assets')));
+
+//const db = require("models");
+//db.sequelize.sync({ force: true }).then(() => {
+//    console.log("Drop and re-sync db.");
+//});
 
 // simple route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
-require("./app/routes/turorial.routes")(app);
+require("./app/routes/exit.routes")(app);
+require("./app/routes/location.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
